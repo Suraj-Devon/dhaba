091ecf94e276c4e97ece8c8007db50554ac435f2
Suraj-Devon <surajr8800@gmail.com>
2024-10-15 23:07:04 +0530

    Update vite.config.js to set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Mouthful-Magic/', // Update this to match your GitHub repo name
+  base: '/Mouthful-Magic/', // This should match your GitHub repo name
   plugins: [react()],
-});
+})
+
